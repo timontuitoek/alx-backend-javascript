c2c02f01e8326f1ab8409d562c3c119192cfd3c5
timontuitoek <tuitoektimon@gmail.com>
2024-03-04 23:43:15 +0300

    solving errors

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,17 @@
+// 0-constants.js
+
 export function taskFirst() {
-    const task = 'I prefer const when I can.'; // Use const to instantiate variable
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let'; // Use let to instantiate variable
-    combination += getLast();
-  
-    return combination;
-  }
-  
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
