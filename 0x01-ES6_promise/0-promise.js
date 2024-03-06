54051dc3624adf3157c30e2d05fc02fc5f7eabc8
timontuitoek <tuitoektimon@gmail.com>
2024-03-06 20:02:28 +0300

    Return a Promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,15 @@
+// 0-promise.js
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // Your API call logic goes here
+      // For example, you can use fetch or any other asynchronous operation
+  
+      // Simulating a successful API call after a delay
+      setTimeout(() => {
+        resolve("API response data");
+      }, 1000);
+    });
+  }
+  
+  export default getResponseFromAPI;
+  
