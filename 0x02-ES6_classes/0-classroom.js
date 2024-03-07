6770df0a09cef90287dfccc9d0f9792a8f2757f9
timontuitoek <tuitoektimon@gmail.com>
2024-03-07 23:00:36 +0300

    Implementing a class named ClassRoom

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,8 +1,9 @@
 // ClassRoom.js
 
+/* eslint-disable max-classes-per-file */
+
 export default class ClassRoom {
     constructor(maxStudentsSize) {
       this._maxStudentsSize = maxStudentsSize;
     }
   }
-  
