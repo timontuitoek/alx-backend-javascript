9ad812e51c52d7d0ffef7dded68377675da018c0
timontuitoek <tuitoektimon@gmail.com>
2024-03-07 10:52:37 +0300

    using function prototype

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,15 @@
+// 2-then.js
+function handleResponseFromAPI(promise) {
+    return promise
+      .then((result) => {
+        console.log("Got a response from the API");
+        return { status: 200, body: 'success' };
+      })
+      .catch((error) => {
+        console.log("Got a response from the API");
+        return new Error();
+      });
+  }
+  
+  export default handleResponseFromAPI;
+  
