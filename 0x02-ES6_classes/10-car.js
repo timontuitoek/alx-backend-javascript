c3d6e25500284ca739a64d83e189ede449d1c244
timontuitoek <tuitoektimon@gmail.com>
2024-03-08 17:24:56 +0300

    attributed and methods

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,32 +1,11 @@
-// Car.js
-
-class Car {
+export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
-  // Getter for brand
-  get brand() {
-    return this._brand;
-  }
-
-  // Getter for motor
-  get motor() {
-    return this._motor;
-  }
-
-  // Getter for color
-  get color() {
-    return this._color;
-  }
-
-  // Method to clone the car
   cloneCar() {
-    // Create a new instance of the class with the same attributes
-    return new Car(this._brand, this._motor, this._color);
+    return new this.constructor(this._brand, this._motor, this._color);
   }
 }
-
-export default Car;
