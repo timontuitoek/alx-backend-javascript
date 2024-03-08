916883c75b6b7c43bfa20933a36df6fcd5c42f51
timontuitoek <tuitoektimon@gmail.com>
2024-03-08 17:00:07 +0300

    new constructor attributes

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,25 +1,27 @@
-// Airport.js
-
-class Airport {
-  constructor(name, code) {
-    this._name = name;
-    this._code = code;
-  }
-
-  // Getter for name
-  get name() {
-    return this._name;
-  }
-
-  // Getter for code
-  get code() {
-    return this._code;
-  }
-
-  // Default string description
-  toString() {
-    return this._code;
-  }
+export default class Airport {
+    constructor(name, code) {
+      if (typeof name !== 'string' || typeof code !== 'string') throw new Error();
+      this._name = name;
+      this._code = code;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(value) {
+      this._name = value;
+    }
+  
+    get code() {
+      return this._code;
+    }
+  
+    set code(value) {
+      this._code = value;
+    }
+  
+    get [Symbol.toStringTag]() {
+      return this.code;
+    }
 }
-
-export default Airport;
