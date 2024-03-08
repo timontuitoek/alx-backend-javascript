aaa72d7dced0cbae585ac68376eae69cfea0c685
timontuitoek <tuitoektimon@gmail.com>
2024-03-08 15:22:47 +0300

    Currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,33 @@
+// Currency.js
+
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter and Setter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  // Getter and Setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // Method to display full currency format
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
