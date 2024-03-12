7ff07ad994cb121f478d727f3221f5fec34b1af2
timontuitoek <tuitoektimon@gmail.com>
2024-03-12 23:14:18 +0300

    a function getStudentIdsSum that returns the sum of all the student ids

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+function getStudentIdsSum(studentsArray) {
+  if (!Array.isArray(studentsArray)) {
+    return 0;
+  }
+
+  // Use the reduce function to sum up student ids
+  const sumOfIds = studentsArray.reduce((accumulator, student) => accumulator + student.id, 0);
+
+  return sumOfIds;
+}
+
+// Export the function
+module.exports = getStudentIdsSum;
