71ebc256bd1d9be9b5a92cbf4e1cd8a8a476e906
timontuitoek <tuitoektimon@gmail.com>
2024-05-06 17:07:03 +0300

    Unittests in JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,9 +1,5 @@
-/**
- * Prints the given message to STDOUT.
- * @param {string} message The message to display.
- */
-function displayMessage(message) {
-  console.log(message);
+function displayMessage(string) {
+  console.log(string);
 }
 
 module.exports = displayMessage;
